f1fb36ee4212878edf0d14e0eddeb467bb039e94
isZXY <xuanyu0827@gmail.com>
2023-11-25 16:54:07 +0800

    Fix Bug

--- src/util/logging.cc
+++ src/util/logging.cc
@@ -7,15 +7,14 @@ class LoggingReceiver{
     private:
         std::ofstream logfile;
     public:
-        uint64_t receive_timestamp;
-        uint32_t frame_no;
-        uint16_t fragment_no;
-        size_t required_next_frame_no;
-        uint32_t time_since_last;
-        uint32_t avg_EWMA_delay;
+        uint64_t receive_timestamp{0};
+        uint32_t frame_no{0};
+        uint16_t fragment_no{0};
+        size_t required_next_frame_no{0};
+        uint32_t time_since_last{0};
+        uint32_t avg_EWMA_delay{0};
 
-        LoggingReceiver(const std::string& filePath) {
-            logfile.open(filePath, std::ios::out | std::ios::trunc);
+        LoggingReceiver(const std::string& filePath) : logfile(filePath, std::ios::out | std::ios::trunc){
             logfile << "包接收时间戳,帧(Frame)序号,片(Fragment)序号,当前待解码帧序号,包处理时延,EWMA时延\n";
         }
 
@@ -25,12 +24,12 @@ class LoggingReceiver{
         }
 
         void reset_value() {
-            receive_timestamp = -1;
-            frame_no =-1;
-            fragment_no =-1;
-            required_next_frame_no=-1;
-            time_since_last=-1;
-            avg_EWMA_delay=-1;
+            receive_timestamp = 0;
+            frame_no =0;
+            fragment_no =0;
+            required_next_frame_no=0;
+            time_since_last=0;
+            avg_EWMA_delay=0;
         }
 
         void write_line(){
