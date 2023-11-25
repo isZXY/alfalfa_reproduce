f936008ecc47bc2d2cb1f4d9ea399810ae70d6bc
isZXY <xuanyu0827@gmail.com>
2023-11-25 19:42:49 +0800

    First Successful attempt to capture log

--- src/util/logging.cc
+++ src/util/logging.cc
@@ -2,37 +2,27 @@
 # include<fstream>
 # include<string>
 # include <cstdlib>
+# include "logging.hh"
 
-class LoggingReceiver{
-    private:
-        std::ofstream logfile;
-    public:
-        uint64_t receive_timestamp{0};
-        uint32_t frame_no{0};
-        uint16_t fragment_no{0};
-        size_t required_next_frame_no{0};
-        uint32_t time_since_last{0};
-        uint32_t avg_EWMA_delay{0};
 
-        LoggingReceiver(const std::string& filePath) : logfile(filePath, std::ios::out | std::ios::trunc){
-            logfile << "包接收时间戳,帧(Frame)序号,片(Fragment)序号,当前待解码帧序号,包处理时延,EWMA时延\n";
-        }
+LoggingReceiver::LoggingReceiver(const std::string& filePath) : logfile(filePath, std::ios::out | std::ios::trunc){
+    logfile << "包接收时间戳,帧(Frame)序号,片(Fragment)序号,当前待解码帧序号,包处理时延,EWMA时延\n";
+}
 
-        ~LoggingReceiver() {
-            if (logfile.is_open())
-                logfile.close();
-        }
+LoggingReceiver::~LoggingReceiver() {
+    if (logfile.is_open())
+        logfile.close();
+}
 
-        void reset_value() {
-            receive_timestamp = 0;
-            frame_no =0;
-            fragment_no =0;
-            required_next_frame_no=0;
-            time_since_last=0;
-            avg_EWMA_delay=0;
-        }
+void LoggingReceiver::reset_value() {
+    receive_timestamp = 0;
+    frame_no =0;
+    fragment_no =0;
+    required_next_frame_no=0;
+    time_since_last=0;
+    avg_EWMA_delay=0;
+}
 
-        void write_line(){
-            logfile << receive_timestamp << "," <<frame_no << "," <<fragment_no << "," <<required_next_frame_no<< "," <<time_since_last<<","<<avg_EWMA_delay <<"\n";
-        }
-};
+void LoggingReceiver::write_line(){
+    logfile << receive_timestamp << "," <<frame_no << "," <<fragment_no << "," <<required_next_frame_no<< "," <<time_since_last<<","<<avg_EWMA_delay <<"\n";
+}
--- src/util/logging.hh
+++ src/util/logging.hh
@@ -1,27 +1,33 @@
-#ifndef LOGGINGRECEIVER_H
-#define LOGGINGRECEIVER_H
+#ifndef LOGGINGRECEIVER_HH
+#define LOGGINGRECEIVER_HH
 
-#include <iostream>
 #include <fstream>
 #include <string>
-#include <cstdlib>
 
 class LoggingReceiver {
 private:
     std::ofstream logfile;
 
 public:
-    uint64_t receive_timestamp;
-    uint32_t frame_no;
-    uint16_t fragment_no;
-    size_t required_next_frame_no;
-    uint32_t time_since_last;
-    uint32_t avg_EWMA_delay;
+    uint64_t receive_timestamp{0};
+    uint32_t frame_no{0};
+    uint16_t fragment_no{0};
+    size_t required_next_frame_no{0};
+    uint32_t time_since_last{0};
+    uint32_t avg_EWMA_delay{0};
+
+//    uint64_t receive_timestamp;
+//    uint32_t frame_no;
+//    uint16_t fragment_no;
+//    size_t required_next_frame_no;
+//    uint32_t time_since_last;
+//    uint32_t avg_EWMA_delay;
 
     LoggingReceiver(const std::string& filePath);
     ~LoggingReceiver();
+
     void reset_value();
     void write_line();
 };
 
-#endif // LOGGINGRECEIVER_H
+#endif // LOGGINGRECEIVER_HH
